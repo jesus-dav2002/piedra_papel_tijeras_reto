36623ea7337ab1724abf9a08e19ac783b5c5f3fa
David Maldonado <jdmg_1010@hotmail.com>
2020-11-14 13:30:47 -0400

    reto troll

--- ppt.js
+++ ppt.js
@@ -1,21 +1,19 @@
 var user;
 var botones = document.getElementsByClassName("boton");
-var bot_play = Math.floor(Math.random() * ((3+1)-1)+1);
-console.log(bot_play);
-
+var G;
 for(var i = 0; i < botones.length; i++)
 {
     botones[i].addEventListener("click", selection, false)
 }
 function play() {
-if(user === bot_play)
+if(user === G)
 {
     alert("empate");
 }
 else{
-var confrontation = (user === 1 && bot_play === 3) || 
-(user === 2 && bot_play === 1) || 
-(user === 3 && bot_play === 2) ? "Ganasteeeee!!" : "perdiste, intenta otra vez con el boton F5";
+var confrontation = (user === 1 && G === 3) || 
+(user === 2 && G === 1) || 
+(user === 3 && G === 2) ? "Ganasteeeee!!" : "perdiste, intenta otra vez con el boton F5";
 alert(confrontation);
     }
 }
@@ -26,22 +24,24 @@ function selection()
    {
     case "piedra_id":
         user = 1 ;
+        G = 2
         console.log(user);
         play();
         break;
 
     case "papel_id":
         user = 2 ;
+        G = 3 ;
         console.log(user);
         play();
         break;
 
     case "tijeras_id" :
         user = 3 ;
+        G = 1 ;
         console.log(user);
         play();
         break;
 
    }
 }
-console.log(bot_play);
